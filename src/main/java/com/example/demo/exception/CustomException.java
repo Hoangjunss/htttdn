72e02d6334c992fa2e9b410550c726672f9eaca9
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2025-05-07 14:36:26 +0700

    exception

---
+++ src/main/java/com/example/demo/exception/CustomException.java
@@ -0,0 +1,17 @@
+package com.example.demo.exception;
+
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private  final Error error;
+
+    public CustomException(Error error){
+        super(error.getMessage());
+        this.error = error;
+    }
+
+
+
+}
---
+++ src/main/java/com/example/demo/exception/TokenExpiredException.java
@@ -0,0 +1,11 @@
+package com.example.demo.exception;
+
+
+import org.springframework.security.core.AuthenticationException;
+
+// TODO: Lỗi này dùng cho trường hợp các JWT Token bị hết hạn sử dụng
+public class TokenExpiredException extends AuthenticationException {
+    public TokenExpiredException(String msg) {
+        super(msg);
+    }
+}
